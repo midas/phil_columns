67165cfea7450ffa8ac3a0c295a00233eb9712fb
Jason Harrelson <jason@lookforwardenterprises.com>
2014-09-27 23:13:50 -0500

    Always ensure schema_seeds table before using it.

--- lib/phil_columns/archivist.rb
+++ lib/phil_columns/archivist.rb
@@ -8,21 +8,25 @@ module PhilColumns
     end
 
     def clear_seeds
+      ensure_schema_seeds_table!
       raise( *error ) unless backend_responds?( :clear_seeds )
       backend.send :clear_seeds
     end
 
     def record_seed( version )
+      ensure_schema_seeds_table!
       raise( *error ) unless backend_responds?( :record_seed )
       backend.send :record_seed, version
     end
 
     def remove_seed( version )
+      ensure_schema_seeds_table!
       raise( *error ) unless backend_responds?( :remove_seed )
       backend.send :remove_seed, version
     end
 
     def seed_already_executed?( version )
+      ensure_schema_seeds_table!
       raise( *error ) unless backend_responds?( :seed_already_executed? )
       backend.send :seed_already_executed?, version
     end
